52d70a27d3af9b981b9967894dceff0b18fa2a14
rdewan05 <85671134+rdewan05@users.noreply.github.com>
2021-10-12 02:18:11 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+setTimeout(() => {
+    console.log('hello1');
+}, 2000);
+
+console.log("Hi");
+
+setTimeout(() => {
+    console.log('hello2');
+}, 2000);
+
+setTimeout(() => {
+    console.log('hello3');
+}, 2000);
